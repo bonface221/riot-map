07af866bffb62fe1605e89fa9f529b7a54c20216
carolyne <pcmaunda@gmail.com>
2022-06-16 20:30:37 +0300

    model fields for location

--- base/models.py
+++ base/models.py
@@ -6,6 +6,8 @@ class Location(models.Model):
     name = models.CharField(max_length=30, null=True)
     image = models.ImageField(upload_to='mapImages', blank=True)
     description = models.CharField(max_length=30, null=True)
+    latitude = models.FloatField(min_value=-90, max_value=90)
+    longitude = models.FloatField(min_value=-180, max_value=180)
     
     def __str__(self):
         return f'{self.name} Location'
@@ -17,4 +19,3 @@ class Account(models.Model):
 
    
 
-    

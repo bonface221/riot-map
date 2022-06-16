6f035c282b64c5345672fd4e873dd1f2738f1123
carolyne <pcmaunda@gmail.com>
2022-06-16 10:23:21 +0300

    created class for location

--- base/models.py
+++ base/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=30, null=True)
+    image = models.ImageField(upload_to='mapImages', blank=True)
+    description = models.CharField(max_length=30, null=True)
+    
+    def __str__(self):
+        return f'{self.name} Location'
+
+class Account(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30, null=True)
+    last_name = models.CharField(max_length=30, null=True)
+
+    def __str__(self):
+        return f'{self.user} Account'
+
+    
